64f071f00e8abe1dd0b554cccfbd8604ba2cbc8f
lonegunman <lonegunmanb@hotmail.com>
2018-10-08 12:24:05 +0800

    add IsMap check and test

--- mapfunk.go
+++ mapfunk.go
@@ -1,9 +1,21 @@
 package gofunkex
 
+import "reflect"
+
 type MapFunk struct {
 	Arr interface{}
 }
 
 func NewMapFunk(arr interface{}) MapFunk {
+	if !isMap(arr) {
+
+	}
 	return MapFunk{arr}
+}
+func isMap(arr interface{}) bool {
+	if arr == nil {
+		return false
+	}
+	kind := reflect.TypeOf(arr).Kind()
+	return kind == reflect.Map
 }
